f26b85dc68dbf003e8ebbc190700abf81c4f60ca
QuuR32 <quurnet@gmail.com>
2017-11-07 17:10:13 +0100

    changing ulreencoding for database purpose

--- js/app/app.js
+++ js/app/app.js
@@ -1,7 +1,6 @@
-const AJAX_BASE_URL = 'http://atelierlibre.url.ph/app/JavascriptComponents_WS/';
-//const AJAX_BASE_URL = '/JavascriptComponents_WS/';
+// const AJAX_BASE_URL = 'http://atelierlibre.url.ph/app/JavascriptComponents_WS/';
+const AJAX_BASE_URL = '/JavascriptComponents_WS/';
 
-// Creation of data model
 Ext.define('MyApp.model.News', {
 	extend : 'Ext.data.Model',
 	fields : [{
@@ -49,45 +48,56 @@ Ext.onReady(function() {
 	    }
 	});
 	
-	// Ext.Msg.alert('Are you ready?', 'I\'m fucking ready!');
-
     Ext.create('Ext.Button', {
-       renderTo: Ext.getElementById('msgBox'),
-       text: 'Click Me',
-       listeners: {
-          click: function() {
-             Ext.MessageBox.confirm(
-                'Confirm', 'Are you sure you want to do this ?', callbackFunction);
-             function callbackFunction(btn) {
-                if(btn == 'yes') {
-                   Ext.Msg.alert ('Button Click', 'You clicked the Yes button');
-                } else {
-                   Ext.Msg.alert ('Button Click', 'You clicked the No button');
-                }
-             };
-          }
-       }
-    });
+    		renderTo: 'butAjouterNews',
+    		text: 'Ajouter une news',
+    		listeners: {
+    			click: function() {
+    				let emptyRecord = Ext.create('Ext.data.Record', {
+    					ID: -1,
+    					Titre: "",
+    					Contenu: "",
+    					Date: ""
+    				});
+    				winDisplayNews.down('form').getForm().loadRecord(emptyRecord);
+    				winDisplayNews.show();
+    			}
+    		}
+	});
 });
 
 function populateGrid(grid) {
-	// Creation of first grid
-	Ext.create('Ext.grid.Panel', {
-		id : 'gridId',
+	let gridNews = Ext.create('Ext.grid.Panel', {
+		id : 'gridNews',
 		store : grid,
 		stripeRows : true,
 		title : 'News',
-		renderTo : 'gridDiv',
+		renderTo : 'divGridNews',
 		width : '80%',
 		collapsible : true,
 		enableColumnMove : true,
 		enableColumnResize : true,
+		pageSize: 5,
+		listeners: {
+			itemclick: function(dv, record, item, index, e) {
+				if(!record.forDelete) {
+					winDisplayNews.down('form').getForm().loadRecord(record);
+					winDisplayNews.show();
+				}
+			}
+		},
 
 		columns : [
 			{
+				header : "Date",
+				dataIndex : 'Date',
+				flex : .3,
+				sortable : true,
+				hideable : true,
+				align: 'left'
+			},{
 				header : "Titre",
 				dataIndex : 'Titre',
-				id : 'ID',
 				flex : .5,
 				sortable : true,
 				hideable : false
@@ -100,12 +110,136 @@ function populateGrid(grid) {
 				hideable : true
 			},
 			{
-				header : "Date",
-				dataIndex : 'Date',
-				flex : .3,
-				sortable : true,
+				header: '',
+				dataIndex : 'delete',
+				flex : .05,
+				sortable : false,
 				hideable : true,
-				align: 'right'
+				align: 'right',
+				renderer: function (value, metaData, record, row, col, store, gridView) {
+					setTimeout(function() {	
+						var button = Ext.create('Ext.button.Button', {
+							renderTo: 'butDeleteNews_' + record.data.ID,
+					    		text: 'x',
+					    		style: {
+					    			backgroundColor: 'Crimson'
+					    		}
+						});
+						button.on('click', function(e) {
+							record.forDelete = true;
+							
+							Ext.Ajax.request({
+							    url: AJAX_BASE_URL + 'news.php',
+							    method: 'POST',
+							    params: {
+							    		m : 'delete',
+							    		ID: record.data.ID
+							    },
+							    contentType: "application/json; charset=utf-8",
+							    success: function(result) {
+								    store.remove(record);
+								    store.reload();
+							    },
+							    failure: function(response, opts) {
+									Ext.Msg.alert('Erreur', 'Une erreur s\'est produite lors de votre enrregistrement, merci de réessayer ultérieurement.');
+							    }
+							});
+			    			});
+					}, 1);
+					return '<div id="butDeleteNews_' + record.data.ID + '"></div>';
+				}
 			} ]
 	});
-}
+
+	Ext.getCmp('gridNews').store.reload();
+};
+
+var winDisplayNews = new Ext.Window({
+	title : 'News',
+	layout : 'form',
+	width : '50%',
+	closeAction : 'close',
+	target : document.getElementById('divWinDisplayNews'),
+	plain : false,
+
+	items : [ {
+        xtype:'form',
+		items : [ {
+				xtype : 'hidden',
+				fieldLabel : 'id',
+				name : 'id'
+			}, {
+		        	xtype : 'hidden',
+				fieldLabel : 'ID',
+				name : 'ID'
+			}, {
+		        	xtype : 'hidden',
+				fieldLabel : 'Date',
+				name : 'Date'
+			}, {
+				xtype : 'textfield',
+				fieldLabel : 'Titre',
+				name : 'Titre',
+				width: '100%'
+			}, {
+				xtype : 'textarea',
+				fieldLabel : 'Contenu',
+				name : 'Contenu',
+				width: '100%',
+				height: '200px'
+			} ]
+	} ],
+
+	buttons : [ {
+		text : 'Enregistrer',
+		handler : function() {
+			let formValues = winDisplayNews.down('form').getForm().getValues();
+			let method = (formValues.ID == "-1" ? 'add' : 'update');
+			Ext.Ajax.request({
+			    url: AJAX_BASE_URL + 'news.php',
+			    method: 'POST',
+			    params: {
+			    		m : method,
+			    		ID: formValues.ID,
+			    		Titre: JSON.stringify(formValues.Titre),
+			    		Contenu: JSON.stringify(formValues.Contenu)
+			    },
+			    contentType: "application/json; charset=utf-8",
+			    success: function(result) {
+			    		let record = {};
+			    		if(result.responseText == "1") {
+			    			// CASE Update
+			    			record = {
+				    			id: formValues.id,
+				    			ID: formValues.ID,
+				    			Titre: formValues.Titre,
+				    			Contenu: formValues.Contenu,
+				    			Date: formValues.Date
+				    		};
+			    		} else {
+			    			// CASE Create
+					    	let news = Ext.util.JSON.decode(result.responseText)[0];
+				        	record = {
+				    			ID: news.news.ID,
+				    			Titre: news.news.Titre,
+				    			Contenu: news.news.Contenu,
+				    			Date: news.news.Date
+				    		};
+			    		}
+			        	Ext.getCmp('gridNews').store.add(record);
+			        	Ext.getCmp('gridNews').store.reload();
+					winDisplayNews.close();
+			    },
+			    failure: function(response, opts){
+					Ext.Msg.alert('Erreur', 'Une erreur s\'est produite lors de votre enrregistrement, merci de réessayer ultérieurement.');
+			    }
+			});
+		}
+	}, {
+		text : 'Annuler',
+		handler : function() {
+			winDisplayNews.close();
+		}
+	} ],
+	buttonAlign : 'center'
+});
