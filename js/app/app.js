419eb2a3ef9282618bd9c44abc349dd7a67e278d
QuuR32 <quurnet@gmail.com>
2017-11-07 17:11:16 +0100

    changing ulreencoding for database purpose

--- js/app/app.js
+++ js/app/app.js
@@ -1,5 +1,5 @@
-// const AJAX_BASE_URL = 'http://atelierlibre.url.ph/app/JavascriptComponents_WS/';
-const AJAX_BASE_URL = '/JavascriptComponents_WS/';
+const AJAX_BASE_URL = 'http://atelierlibre.url.ph/app/JavascriptComponents_WS/';
+// const AJAX_BASE_URL = '/JavascriptComponents_WS/';
 
 Ext.define('MyApp.model.News', {
 	extend : 'Ext.data.Model',
